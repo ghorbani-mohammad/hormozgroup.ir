0d7e1fdfe76ebc4eb13580f96e07601446eb2338
Mohammad Ghorbani <aali361@gmail.com>
2020-03-24 07:58:11 -0400

    Improvment on laravel mix (webpack)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,67 +1,45 @@
 const mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 const glob = require('glob-all');
-// require('laravel-mix-imagemin');
-let ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
 
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.webpackConfig( {
+// For compress images
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+mix.webpackConfig({
     plugins: [
+        new CopyWebpackPlugin([{
+            from: 'resources/images',
+            to: 'images', // Laravel mix will place this in 'public/img'
+        }]),
         new ImageminPlugin({
-//            disable: process.env.NODE_ENV !== 'production', // Disable during development
-            pngquant: {
-                quality: '95-100',
-            },
             test: /\.(jpe?g|png|gif|svg)$/i,
-        }),
-    ],
-})
+            plugins: [
+                imageminMozjpeg({
+                    quality: 80,
+                })
+            ]
+        })
+    ]
+});
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css');
+// Css puring
+mix.postCss('public/css/bootstrap.min.css', 'public/css/bootstrap.minified.css')
+.purgeCss();
 
-// mix.postCss('public/css/bootstrap.min.css', 'public/css/bootstrap.minified.css')
-// .purgeCss();
-
-// mix.postCss('public/css/font-awesome.min.css', 'public/css/font-awesome.minified.css')
-// .purgeCss();
-
-mix.copy( 'resources/images', 'public/images', true);
-
-// mix.imagemin('images/*');
+mix.postCss('public/css/font-awesome.min.css', 'public/css/font-awesome.minified.css')
+.purgeCss();
 
 
-// mix.imagemin(
-//     'img/**.*',
-//     {
-//         context: 'resources',
-//     },
-//     {
-//         optipng: {
-//             optimizationLevel: 5
-//         },
-//         jpegtran: null,
-//         plugins: [
-//             require('imagemin-mozjpeg')({
-//                 quality: 100,
-//                 progressive: true,
-//             }),
-//         ],
-//     }
-// );
+// Copy last verseion of Jquery
+mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
 
-// mix.scripts([
-//     'public/js/bootstrap.min.js',
-//     'public/js/jquery-3.2.1.min.js'
-// ], 'public/js/all.js');
+// Combining different javascript libraries
+mix.scripts([
+    'public/js/jquery.min.js',
+    'public/js/popper.min.js',
+    'public/js/bootstrap.min.js',
+    'public/js/jquery.lazy.min.js',
+    'public/js/jquery.lazy.plugins.min.js'
+], 'public/js/all.js');
