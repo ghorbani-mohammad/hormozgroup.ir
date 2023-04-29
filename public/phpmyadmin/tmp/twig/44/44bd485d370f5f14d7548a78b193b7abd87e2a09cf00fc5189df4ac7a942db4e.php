879b063b9f2c40078d4a45bab64d7175ceed1491
Moahammad <mohammad.ghorbani@hotmail.com>
2023-04-29 21:16:20 +0000

    remove phpmyadmin dir from public

---
+++ public/phpmyadmin/tmp/twig/44/44bd485d370f5f14d7548a78b193b7abd87e2a09cf00fc5189df4ac7a942db4e.php
@@ -0,0 +1,72 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* display/export/options_output_radio.twig */
+class __TwigTemplate_86eda5c399a12b2b2accf7e64b081b6e256a541ae6edb9ac3f56c200b3743c72 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<li>
+    <input type=\"radio\" id=\"radio_view_as_text\" name=\"output_format\" value=\"astext\"";
+        // line 3
+        echo (((($context["has_repopulate"] ?? null) || (($context["export_asfile"] ?? null) == false))) ? (" checked") : (""));
+        echo ">
+    <label for=\"radio_view_as_text\">
+        ";
+        // line 5
+        echo _gettext("View output as text");
+        // line 6
+        echo "    </label>
+</li>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "display/export/options_output_radio.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 6,  38 => 5,  33 => 3,  30 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "display/export/options_output_radio.twig", "/usr/share/phpMyAdmin/templates/display/export/options_output_radio.twig");
+    }
+}
