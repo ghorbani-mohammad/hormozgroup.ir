28a77d56a060d725ec8d5b60ca1bc04f6b94c6d4
Moahammad <mohammad.ghorbani@hotmail.com>
2023-04-30 10:55:11 +0000

    add keywords and description to index-post page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,10 @@
 
 @extends('master')
 
-@section('title')
-وبلاگ گروه هرمز
-@endsection
+@section('title', 'وبلاگ || گروه هرمز')
+
+@section('keywords', 'سینی کابل، نصب سینی کابل، اجرای سینی کابل، مقالات سینی کابل')
+@section('description', 'مقالات آموزشی در رابطه با سینی کابل، انواع سینی کابل، نصب سینی کابل و غیر')
 
 @section('container')
     <div style="height:2rem;"></div>
